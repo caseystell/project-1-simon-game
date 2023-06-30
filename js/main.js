c7a70e313088e6053546cfd85f9db84cd9f7abe5
caseystell <cleigh14@hotmail.com>
2023-06-30 12:26:19 -0600

    update css

--- js/main.js
+++ js/main.js
@@ -21,17 +21,17 @@ const colorEls = document.querySelectorAll("#color-buttons > div");
 /* Pseudocode:
 
 const array for the 4 color variables, which link to the HTML divs of the corresponding colors (cached element)
-delcare state variables: noMistakes = true; currentScore = 0, highScore = 0;
+declare state variables: noMistakes = true; currentScore = 0, highScore = 0;
 click event listener to verify if each click matches what color the computer randomly generated
 create a while loop: while noMistakes, execute gamePlay function
     computer randomly selects a color and displays the color, while making the corresponding sound for that color
     user is given time to copy that color
         if user takes longer than 1 minute to select a color, game times out and resets (while loop ends)
-    if user is correct (noMistakes = true), game continues, computer add 1 to its random sequence
+    if user is correct (noMistakes = true), game continues, computer add 1 to its random sequence, ensuring sequence is the same + 1
     if a mistake is made, noMistakes = false, ends while loop, resets board to initial state & currentScore to 0
         if currentScore > highScore, currentScore becomes the new highScore
         highScore is never reset
-create a play again game functionality when the player loses
+create a play again game button functionality when the player loses
 
 
 
