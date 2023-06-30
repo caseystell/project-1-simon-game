525caab32cdc99a8c1ca9e61cee6adbf05606f05
caseystell <cleigh14@hotmail.com>
2023-06-30 15:01:04 -0600

    update html, css and js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,94 @@
 /*----- constants -----*/
 const colors = ["red", "blue", "yellow", "green"];
 
-
 /*----- state variables -----*/
 let noMistakes = true;
 let currentScore = 0;
 let highScore = 0;
+let turnCount = 0;
+let computerSequence = [];
+let playerSequence = [];
 
 
 /*----- cached elements  -----*/
-const colorEls = document.querySelectorAll("#color-buttons > div");
+const curScoreEl = document.getElementById("cur-score");
+const highScoreEl = document.getElementById("high-score");
+const startBtn = document.getElementById("start");
 
 
 /*----- event listeners -----*/
+startBtn.addEventListener("click", init);
+
 
 
 /*----- functions -----*/
+init();
 
+function init() {
+    startBtn.classList.add("hidden");
+    noMistakes = true;
+    currentScore = 0;
+    turnCount = 0;
+    computerSequence = [];
+    playerSequence = [];
+    render();
+};
+
+function render() {
+    renderBoard();
+    renderMessage();
+    renderControls();
+};
+
+function renderBoard() {
+    // playTurn();
+};
+
+function renderMessage() {
+    const messageEl = document.querySelector("h2");
+    if (noMistakes === false) {
+        messageEl.innerText = `Game over! You got ${currentScore} in a row!`
+    } return;
+};
+
+function renderControls() {
+    const playAgainBtn = document.getElementById("replay");
+    playAgainBtn.style.visibility = noMistakes ? "hidden" : "visible";
+};
+
+function playTurn() {
+   
+   
+};
+
+function getRandomColor() {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    return randomColor;
+};
+
+function makeSound(color) {
+    const colorEl = document.querySelector(`[id='${color}']`);
+    const colorSounds = document.querySelector(`[sound='${color}']`);
+    colorEl.classList.add("activated");
+    colorSounds.play();
+    
+    setTimeout(function() {
+        colorEl.classList.remove("activated");
+    }, 500);
+};
+
+function playNextTurn() {
+    getRandomColor();
+    const nextTurnSequence = [...computerSequence];
+    nextTurnSequence.push(getRandomColor());
+    makeSound(nextTurnSequence);
+    turnCount += 1;
+}
 
 /* Pseudocode:
 
-const array for the 4 color variables, which link to the HTML divs of the corresponding colors (cached element)
-declare state variables: noMistakes = true; currentScore = 0, highScore = 0;
+X const array for the 4 color variables, which link to the HTML divs of the corresponding colors (cached element)
+X declare state variables: noMistakes = true; currentScore = 0, highScore = 0;
 click event listener to verify if each click matches what color the computer randomly generated
 create a while loop: while noMistakes, execute gamePlay function
     computer randomly selects a color and displays the color, while making the corresponding sound for that color
@@ -31,14 +98,14 @@ create a while loop: while noMistakes, execute gamePlay function
     if a mistake is made, noMistakes = false, ends while loop, resets board to initial state & currentScore to 0
         if currentScore > highScore, currentScore becomes the new highScore
         highScore is never reset
-create a play again game button functionality when the player loses
+X create a play again game button functionality when the player loses
 
 
 
 
 To figure out:
-import or lookup how to choose items "randomly" from an array
-how to get sounds in js associated to divs/variables
+X import or lookup how to choose items "randomly" from an array
+X how to get sounds in js associated to divs/variables
 how to get computer to generate the same random sequence while no mistakes are made (asynchronous timing?)
 
 */
